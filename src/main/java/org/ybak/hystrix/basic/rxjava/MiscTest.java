6e4842b62abffeb6c0e663f7bb148deab7fa9551
ybak <ybakswu@gmail.com>
2016-06-29 15:02:33 +0800

    添加更多的Rx测试代码.

--- src/main/java/org/ybak/hystrix/basic/rxjava/MiscTest.java
+++ src/main/java/org/ybak/hystrix/basic/rxjava/MiscTest.java
@@ -8,16 +8,18 @@ import rx.Subscriber;
 import rx.Subscription;
 import rx.functions.Action1;
 import rx.internal.util.InternalObservableUtils;
+import rx.observables.GroupedObservable;
 import rx.schedulers.Schedulers;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-import java.util.Random;
+import java.sql.Time;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 /**
  * Created by isaac on 16/6/24.
@@ -40,6 +42,36 @@ public class MiscTest {
     public void test() {
         Stream<Integer> stream = Arrays.asList(1, 2, 3, 4, 5).stream();
         System.out.println(stream.map(Objects::toString).collect(Collectors.joining()));
+    }
+
+    @Test
+    public void windowGroupTest() throws Exception {
+        Observable.interval(1, TimeUnit.MILLISECONDS)
+                .map(x -> RandomUtils.nextInt(10))
+                .buffer(100)
+                .take(3).toBlocking()
+                .subscribe(
+                        buffer -> {
+                            buffer.stream()
+                                    .map(word -> new AbstractMap.SimpleEntry<>(word, 1L))
+                                    .collect(groupingBy(AbstractMap.SimpleEntry::getKey, counting()))
+                                    .entrySet().stream()
+                                    .forEach(System.out::println);
+                            System.out.println();
+                        }
+                );
+    }
+
+    @Test
+    public void testGroup() throws InterruptedException {
+        Observable.interval(1, TimeUnit.MILLISECONDS)
+                .map(x -> RandomUtils.nextInt(10))
+                .take(100)
+                .groupBy(n -> n)
+                .flatMap(g -> g.toList())
+                .map(list -> new AbstractMap.SimpleEntry(list.get(0), list.size()))
+                .toBlocking()
+                .forEach(System.out::println);
     }
 
 }
